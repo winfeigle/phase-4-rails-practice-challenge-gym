310ed4e577527663af3e73ab059fa0d38bf72e46
Win Feigle <win.feigle@gmail.com>
2023-01-06 12:22:01 -0700

    Add error handling for memberships

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,4 +1,6 @@
 class MembershipsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
     def create
         new_membership = Membership.create!(membership_params)
         render json: new_membership, status: :created
@@ -8,5 +10,9 @@ class MembershipsController < ApplicationController
 
     def membership_params
         params.permit(:charge, :gym_id, :client_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
